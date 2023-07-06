bc97a679ba76c7937a9f7307f23ba28fcf4a6cbf
Anas Bensaid <bensaidanass9@gmail.com>
2023-07-06 17:40:32 +0100

    Customazing Exceptions

--- src/main/java/com/example/jwt/auth/AuthenticationController.java
+++ src/main/java/com/example/jwt/auth/AuthenticationController.java
@@ -1,7 +1,15 @@
 package com.example.jwt.auth;
 
+import com.example.jwt.config.JwtService;
+import com.example.jwt.user.User;
+import com.example.jwt.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -10,19 +18,47 @@ import org.springframework.web.bind.annotation.*;
 public class AuthenticationController {
 
     private final AuthenticationService service;
+    private final UserRepository userRepository;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
 
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
         return ResponseEntity.ok(service.register(request));
     }
 
+//    @PostMapping("/authenticate")
+//    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
+//        return ResponseEntity.ok(service.authenticate(request));
+//    }
+
     @PostMapping("/authenticate")
-    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
-        return ResponseEntity.ok(service.authenticate(request));
+    public ResponseEntity<?> authenticate(@RequestBody AuthenticationRequest request) {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            request.getEmail(),
+                            request.getPassword()
+                    )
+            );
+        } catch (BadCredentialsException ex) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid credentials");
+        } catch (DisabledException ex) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Disabled Account");
+        }
+
+        // Retrieve the user from the repository
+        var user = userRepository.findByEmail(request.getEmail()).orElse(null);
+        var jwtToken = jwtService.generateToken(user);
+        var response = AuthenticationResponse.builder()
+                .token(jwtToken)
+                .expirationDate(jwtService.extractExpiration(jwtToken))
+                .role(user.getRole().name())
+                .isActive(user.isActive())
+                .build();
+
+        return ResponseEntity.ok(response);
     }
 
-    @GetMapping("/demo")
-    public ResponseEntity<String> sayHello() {
-        return ResponseEntity.ok("Hello from secured endpoint");
-    }
 }
